e3cdd6912345c22f79577776df6eb475db775358
Beka Westberg <bekawestberg@gmail.com>
2019-12-12 11:38:08 -0800

    style: Fixed license year.

--- test/src/main/java/com/bekawestberg/loopinglayout/test/AdapterGeneric.kt
+++ test/src/main/java/com/bekawestberg/loopinglayout/test/AdapterGeneric.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Looping Layout
+ * Copyright 2019 Looping Layout
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
