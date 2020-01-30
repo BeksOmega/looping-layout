b77afa9a2011c14a06fee26d4de1f362f4b5d7de
Beka Westberg <bekawestberg@gmail.com>
2020-01-30 15:19:48 -0800

    feat: Added temporary snap helper support. (#27)
    * Updated gradle.
    
    * Added temporary snap helper.
    
    * Optimized imports.

--- test/src/main/java/com/bekawestberg/loopinglayout/test/ActivityVertical.kt
+++ test/src/main/java/com/bekawestberg/loopinglayout/test/ActivityVertical.kt
@@ -18,20 +18,20 @@
 package com.bekawestberg.loopinglayout.test
 
 import android.os.Bundle
-
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bekawestberg.loopinglayout.library.LoopingLayoutManager
-import com.bekawestberg.loopinglayout.library.addViewsAtOptAnchorEdge
+import com.bekawestberg.loopinglayout.library.LoopingSnapHelper
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ActivityVertical : AppCompatActivity() {
     private lateinit var mRecyclerView: RecyclerView
     private var mAdapter: AdapterGeneric = AdapterGeneric(
-            arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"))
+            Array(16) { i -> i.toString()},
+            Array(16) { i -> 250})
     private var mLayoutManager: RecyclerView.LayoutManager =
-            LoopingLayoutManager(this, RecyclerView.VERTICAL, true)
+            LoopingLayoutManager(this, RecyclerView.VERTICAL, false)
+    private var snapHelper = LoopingSnapHelper();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +44,8 @@ class ActivityVertical : AppCompatActivity() {
         mRecyclerView.layoutManager = mLayoutManager
         mRecyclerView.adapter = mAdapter
         //mLayoutManager.smoothScrollDirectionDecider = ::addViewsAtOptAnchorEdge
+
+        snapHelper?.attachToRecyclerView(mRecyclerView)
 
         val button = findViewById<FloatingActionButton>(R.id.fab)
         button.setOnClickListener { mAdapter.notifyDataSetChanged() }
