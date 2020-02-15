fba80ebf433383c3eea084df86d81fe356199916
Beka Westberg <bekawestberg@gmail.com>
2020-02-15 15:22:12 -0800

    style: Match kotlin doc conventions.

--- library/src/main/java/com/bekawestberg/loopinglayout/library/LoopingSnapHelper.kt
+++ library/src/main/java/com/bekawestberg/loopinglayout/library/LoopingSnapHelper.kt
@@ -3,8 +3,16 @@ package com.bekawestberg.loopinglayout.library
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 
-class LoopingSnapHelper : LinearSnapHelper() {
-    override fun findTargetSnapPosition(
+/**
+ * A snap helper built specifically for the [LoopingLayoutManager].
+ */
+public class LoopingSnapHelper : LinearSnapHelper() {
+
+    /**
+     * Returns [RecyclerView.NO_POSITION] so that the snap helper is forced to use the default
+     * animation logic.
+     */
+    public override fun findTargetSnapPosition(
             layoutManager: RecyclerView.LayoutManager?,
             velocityX: Int,
             velocityY: Int
